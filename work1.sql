5039c4eb503c32d80a43c6a58bf0d11f380ae4b0
920408194 <13217322877@163.com>
2018-05-01 17:25:47 +0800

    Update work1.sql

--- work1.sql
+++ work1.sql
@@ -65,3 +65,74 @@
 		from (select count(employee) as employee_count,company_name
 					from works);
 	--g
+		select company_name
+		from works
+		where salary > (select avg(salary)
+										from works);
+--3.10
+	--a
+		update employee
+			set city = 'Newtown'
+			where employee_name = 'Jones';
+		update employee
+			set street = null
+			where employee_name = 'Jones';
+	--b
+		update works
+			set salary = case
+				when salary >100000 then salary * 1.03
+				else salary * 1.10;
+--3.11
+	--a
+
+
+--3.13
+		create table person (
+			driver_id  varchar(20),
+			name  varchar(20) not null,
+			address  varchar(1111),
+			primary key (driver_id));
+
+		create table car(
+			license  varchar(20),
+			model  varchar(20),
+			year  varchar(20),
+			primary key (license));
+
+		create table accident(
+			report_number  varchar(20),
+			date  varchar(20),
+			location  varchar(1111),
+			primary key (report_number));
+
+		create table owns(
+			driver_id  varchar(20),
+			license  varchar(20),
+			primary key (driver_id),
+			foregin key (driver_id) references person,
+			foregin key (license) references car);
+
+		create table participated(
+			report_number varchar(20),
+			license  varchar(20),
+			driver_id varchar(20),
+			damage_amount numeric(8,2)
+			primary key (report_number,license),
+			foregin key (report_number) references accident,
+			foregin key (license) references car);
+--3.14
+	--a
+		select count(*)
+		from accident natural join participated
+		where license in (select license
+											from car natural join owns natural join person
+											where name = 'John Smith');
+	--b
+		update participated
+			set damage_amount = 3000
+			where report_number = 'AR2197'
+			and license = 'AABB2000';
+--3.15
+	--a 
+		select customer_name
+		from 
